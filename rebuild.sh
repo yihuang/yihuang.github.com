a4e3cf6a7d33448708ad3c0632963e5755ba5d46
yihuang <yi.codeplayer@gmail.com>
2010-10-07 17:40:22 +0800

    style fix

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,2 @@
-#!/usr/bin/expect -f
-spawn ssh yiprogra@yi-programmer.com
-set timeout 60
-expect "yiprogra@yi-programmer.com *"
-send "/home2/yiprogra/articles/crontab_run.sh\r"
-set timeout 120
-expect "yiprogra@yi-programmer.com *"
-send "exit\r"
-expect eof
+#!/bin/sh
+ssh blog "/home2/yiprogra/articles/crontab_run.sh"
