7b9958c1d2bd985e4267d0bd2baca38f69268f89
huangyi <yi.codeplayer@gmail.com>
2011-05-21 21:56:07 +0800

    remove codingproj

--- codingproj/dev_settings.py
+++
@@ -1,8 +0,0 @@
-from common_settings import *
-
-MODE = 'dev'
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
-STATIC_URL = '/static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL
--- codingproj/snsauth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from views import *
-
-urlpatterns = patterns('snsauth.views',
-    url(r'^douban/$', DoubanLoginView.as_view(), name='douban_login'),
-    url(r'^sina/$', SinaLoginView.as_view(), name='sina_login'),
-    url(r'^qq/$', QQLoginView.as_view(), name='qq_login'),
-)
